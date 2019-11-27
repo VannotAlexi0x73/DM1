a560e7b545727bf80828f0894bfb7307bf14b07b
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2019-11-27 01:01:55 +0100

    [FIX]

--- src/com/jeu_pion/Grille.java
+++ src/com/jeu_pion/Grille.java
@@ -3,9 +3,9 @@ package com.jeu_pion;
 public class Grille {
     private char value;
     private char status;
-    
+
     public void SetValue(char value) { this.value = value; }
     public void SetStatus(char status) { this.status = status; }
-    public char GetValue(char value) { return this.value; }
-    public char GetStatus(char status) { return this.status; }
+    public char GetValue() { return this.value; }
+    public char GetStatus() { return this.status; }
 }
