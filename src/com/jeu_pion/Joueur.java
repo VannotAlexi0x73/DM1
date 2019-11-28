493f891af729d87a3a19403787d59fd2e51863b5
VannotAlexi0x73 <alexis.vannot@outlook.fr>
2019-11-28 22:08:52 +0100

    [IMP] method comment

--- src/com/jeu_pion/Joueur.java
+++ src/com/jeu_pion/Joueur.java
@@ -4,10 +4,33 @@ public class Joueur {
     private String prenom;
     private String caractere;
 
+    /**
+     * Permet de créer un Joueur en renseignant directement son prenom
+     * @param prenom : prenom du Joueur
+     */
     public Joueur(String prenom) { this.prenom = prenom; }
 
+    /**
+     * Permet de changer le prénom d'un Joueur
+     * @param prenom : nouveau prenom du Joueur
+     */
     public void EnregistrerPrenom(String prenom) { this.prenom = prenom; }
+
+    /**
+     * Méthode qui enregistre le caractère que le joueur a choisi pour ses "pions"
+     * @param caractere :
+     */
     public void EnregistrerCaractere(String caractere) { this.caractere = caractere; }
-    public String ObtenirPrenom(String prenom) { return this.prenom; }
-    public String ObtenirCaractere(String caractere) { return this.caractere; }
+
+    /**
+     * Permet d'obtenir le nom du Joueur
+     * @return : prenom du Joueur
+     */
+    public String ObtenirPrenom() { return this.prenom; }
+
+    /**
+     * Permet d'obtenir le caractère que le Joueur a choisi
+     * @return : caractère du Joueur
+     */
+    public String ObtenirCaractere() { return this.caractere; }
 }
