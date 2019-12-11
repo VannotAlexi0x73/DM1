6c51dd21de665f885515ff11c9a65a6ff3f87a3a
moiRemi <58235111+moiRemi@users.noreply.github.com>
2019-12-11 20:01:56 +0100

    puissance 4 méthode jouer et gestion position
    ( à test )

--- src/com/jeu_pion/Puissance4.java
+++ src/com/jeu_pion/Puissance4.java
@@ -16,11 +16,37 @@ public class Puissance4 {
 	
 	public void jouer(Joueur joueur) {
 		
+		int positionPion;
+		
+		do 
+		{
+			System.out.println( joueur.getPrenom() + " Ã  toi de jouer");
+			System.out.println("place ton pion (ex : ");
+			positionPion = scanner.nextInt();
+		}
+		while(gestionPosition(positionPion, joueur.getCaractere()) == false);
 	}
 	
 	
-	public void gestionPosition() {
+	public boolean gestionPosition(int position , String caractere) {
+		int i = 0;
+		if ((position>=0)&&(position< plateau.getColonneMatricePlateau())) {
+			if (plateau.obtenirValeurMatricePlateau(0,position) == null) {
+				do {
+					i++;
+				}while(plateau.obtenirValeurMatricePlateau( i+1, position) == null);
+				plateau.modifierMatricePlateau( i, position, caractere);
+				return true;
 		
+			}else {
+				System.out.println( "la colone est pleine");
+				return false;
+			}
+		}
+		else {
+			System.out.println( "la colone n'existe pas");
+			return false;
+		}
 	}
 	
 	
@@ -28,12 +54,12 @@ public class Puissance4 {
 	public int verification(Joueur joueur) {
 		int i = 0;
 		
-		// Si le joueur a gagné on renvoie XXX
-		if (/* Nb pions alignés == 4 */) {
+		// Si le joueur a gagnÃ© on renvoie XXX
+		if (/* Nb pions alignÃ©s == 4 */) {
 			i = 1;
 			return i ;
 		}
-		// Si il y a égalité alors on renvoie XXX
+		// Si il y a Ã©galitÃ© alors on renvoie XXX
 		else if (/* Plateau plein & aucun gagnant */) {
 			i = 2;
 			return i ;
